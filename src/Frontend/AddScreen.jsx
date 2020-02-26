fdc792f314ceaa90336a054cf2a627dc285b77ad
Yilmaz <yilmaz.koeknar@code.berlin>
2020-02-26 14:49:05 +0100

    first commit

---
+++ src/Frontend/AddScreen.jsx
@@ -0,0 +1,20 @@
+import './AddSreenstyless.css'
+import React, {useContext} from "react";
+
+class AddSreen extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default AddSreen
